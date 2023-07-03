358615d52c4c9875712732da245c4b770138d9e7
Bala Chinnaiah <97093618+BalaChinnaiah@users.noreply.github.com>
2023-07-03 17:08:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+let a = [44,11,1,22,13,50]
+
+// for(let i=0; i<a.length; i++)
+// {
+//     console.log(a[i])
+// }
+
+
+// a.forEach((element) => {
+//     console.log(element*element)
+// })
+
+
+// let name = "Bala Sagar"
+// let arr = Array.from(name)
+// console.log(arr)
+
+
+
+// for(let i of a)
+// {
+//     console.log(i)
+// }
+
+
+// for(let i in a)
+// {
+//     console.log(a[i])
+// }
